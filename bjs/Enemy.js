d4e8a9edaf1f8b4151368daa97f4ce2bf84118d6
BMarcelus <bmdizon@uci.edu>
2018-05-23 22:12:16 -0700

    Enemy lint

--- bjs/Enemy.js
+++ bjs/Enemy.js
@@ -1,6 +1,6 @@
 (function LoadEnemy() {
-	const { Entity, Time, HitBox } = BDOTJS;
-	class Enemy extends Entity {
+  const { Entity, Time, HitBox } = BDOTJS;
+  class Enemy extends Entity {
     constructor(world, ...args) {
       super(...args);
       this.world = world;
@@ -35,41 +35,40 @@
       this.applyGravity();
       this.applyVelocity();
       this.world.boundToFloor(this, this.onGrounded);
-		}
-		takeDamage(dmg){
-			if (this.invul) return;
-			this.invul = true;
-			this.color = '#0ff';
-			setTimeout(() => { this.color = '#2af'; }, 50);
-			this.life -= dmg;
-			if (this.life <= 0) {
-				setTimeout(() => { this.shouldDelete = true; }, 200);
-			} else {
-				setTimeout(() => { this.color = 'blue'; this.invul = false; }, 200);
-			}
-		}
-		doKnockback(dir, xForce, yForce) {
-			this.vx = xForce * dir;
+    }
+    takeDamage(dmg) {
+      if (this.invul) return;
+      this.invul = true;
+      this.color = '#0ff';
+      setTimeout(() => { this.color = '#2af'; }, 50);
+      this.life -= dmg;
+      if (this.life <= 0) {
+        setTimeout(() => { this.shouldDelete = true; }, 200);
+      } else {
+        setTimeout(() => { this.color = 'blue'; this.invul = false; }, 200);
+      }
+    }
+    doKnockback(dir, xForce, yForce) {
+      this.vx = xForce * dir;
       this.vy = -1 * yForce;
       this.canMove = false;
       Time.setFramedTimeout(() => {
         this.canMove = true;
       }, 30);
-		}
+    }
     attack() {
       const s = 74;
       const d = ((this.w + s) / 2) * (1 - (2 * this.flipped));
       let x = this.x + (this.w / 2) + d + this.vx;
       x -= s / 2;
-			const y = (this.y + (this.h / 2)) - (s / 2);
-			const hitbox = new HitBox(this, x, y, s, s);
-			hitbox.collisionBehaviour = function(col) {
-				if(col.team === 1)
-      	{
-        	col.takeDamage(1);
-        	col.doKnockback(this.direction, 10, 10);
-				}
-			}
+      const y = (this.y + (this.h / 2)) - (s / 2);
+      const hitbox = new HitBox(this, x, y, s, s);
+      hitbox.collisionBehaviour = function EnemyAttackCollisionBehavior(col) {
+        if (col.team === 1) {
+          col.takeDamage(1);
+          col.doKnockback(this.direction, 10, 10);
+        }
+      };
       this.driver.addEntity(hitbox);
     }
     onGrounded() {
@@ -77,6 +76,6 @@
       this.vx = 0;
     }
   }
-  
-	BDOTJS.Enemy = Enemy;
-  }());
+
+  BDOTJS.Enemy = Enemy;
+}());
