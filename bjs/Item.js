cb65a1fab033c44b5a072dfa13e33375420699e0
BMarcelus <bmdizon@uci.edu>
2018-05-23 22:09:12 -0700

    item lint

--- bjs/Item.js
+++ bjs/Item.js
@@ -1,44 +1,37 @@
 (function LoadItem() {
-  const { Entity, Inventory } = BDOTJS;
+  const { Entity } = BDOTJS;
 
   class ItemObject extends Entity {
-
     constructor(world, data, ...args) {
-			super(...args);
-			this.world = world;
-			this.data = data;
-			this.color = 'red';
-			this.vx = 0;
-			this.vy = 0;
-			this.gravity = 1;
-			this.pickedup = false;
-		}
+      super(...args);
+      this.world = world;
+      this.data = data;
+      this.color = 'red';
+      this.vx = 0;
+      this.vy = 0;
+      this.gravity = 1;
+      this.pickedup = false;
+    }
 
     update() {
-			this.applyGravity();
+      this.applyGravity();
       this.applyVelocity();
       this.world.boundToFloor(this, this.onGrounded);
-		}
-		onCollision(col) {
-			if(!col.inventory) return;
-			if(col.inventory.addItem(this.data))
-				this.shouldDelete = true;
     }
-	}
-	
-	class ItemData {
-		constructor() {
-			
-		}
-		useItem(user) {
-			this.itemBehaviour(user);
-		}
-		itemBehaviour(user)
-		{
-			console.log("default item behaviour");
-		}
-	}
+    onCollision(col) {
+      if (!col.inventory) return;
+      if (col.inventory.addItem(this.data)) {
+        this.shouldDelete = true;
+      }
+    }
+  }
 
-	BDOTJS.ItemData = ItemData;
-	BDOTJS.ItemObject = ItemObject;
+  class ItemData {
+    useItem(user) {
+      this.itemBehaviour(user);
+    }
+  }
+
+  BDOTJS.ItemData = ItemData;
+  BDOTJS.ItemObject = ItemObject;
 }());
